819f4a527fc85c330fddf9596435ab9ee13dc7d9
mikeku <mikekuforgit@gmail.com>
2020-07-18 21:53:40 +0800

    設定PostgreSQL資料庫連線資訊

--- bot/settings.py
+++ bot/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'bot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',  # PostgreSQL
+        'NAME': 'TRIP',  # 資料庫名稱
+        'USER': 'postgres',  # 資料庫帳號
+        'PASSWORD': '****',  # 資料庫密碼
+        'HOST': 'localhost',  # Server(伺服器)位址
+        'PORT': '5432'  # PostgreSQL Port號
     }
 }
 
