96c1659ffee83e1608317f53c0d316a1e90bdba1
mikeku <mikekuforgit@gmail.com>
2020-07-18 22:30:34 +0800

    安裝psycopg2套件及建立景點資料模型

--- triplinebot/models.py
+++ triplinebot/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+    area = models.CharField(max_length=20)  # 地區
+    name = models.CharField(max_length=100)  # 景點名稱
+    address = models.CharField(max_length=500)  # 地址
